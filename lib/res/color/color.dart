9be623952654f204af0e4a4cd96842ff07aec4cb
HYPER <meerhadikhan364@gmail.com>
2022-12-26 14:08:57 +0500

    first commit

---
+++ lib/res/color/color.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class AppColor {
+  //main_button Color
+  static const Color black = Color(0xff000000);
+  // text color
+  static const Color white = Color(0xffffffff);
+  //PersistentBottomNavBar activeColorPrimary
+  static const Color gainsboro = Color(0xffDCDCDC);
+  //PersistentBottomNavBar activeColorSecondary
+  static const Color block = Color(0xff000000);
+}
